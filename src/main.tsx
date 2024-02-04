635dad83f317a913e3978795e96c774400dd1a90
VitaliiR <104318952+Vi7ality@users.noreply.github.com>
2024-02-04 23:44:54 +0200

    Styled component, global style, fonts added

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import App from "./App";
 import "./index.scss";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import { GlobalStyle } from './GlobalStyle';
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
+      <GlobalStyle/>
     </Provider>
   </React.StrictMode>
 );
