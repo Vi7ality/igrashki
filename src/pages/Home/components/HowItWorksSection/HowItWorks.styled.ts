5b03a0d2abd6f7a2636ff5034bde37ee739e3d4d
VitaliiR <104318952+Vi7ality@users.noreply.github.com>
2024-02-07 23:16:58 +0200

    HowItworks styled

--- src/pages/Home/components/HowItWorksSection/HowItWorks.styled.ts
+++ src/pages/Home/components/HowItWorksSection/HowItWorks.styled.ts
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import Container from "../../../../shared/Container";
+import { NavLink } from "react-router-dom";
 
 export const SectionStyled = styled.section`
     padding-top: 140px;
@@ -26,9 +27,29 @@ margin-bottom: 67px;
 export const ListStyled = styled.ul`
 display: flex;
 justify-content: center;
+gap: 102px;
 `
 
 export const ItemStyled = styled.li`
 display: flex;
 flex-direction: column;
+align-items: center;
+width: 330px;
 `
+
+export const SvgStyled = styled.svg`
+width: 210px;
+height: 176px;
+
+margin-bottom: 50px;`
+
+export const TextStyled = styled.p`
+font-size: 18px;
+line-height: 1.185;
+
+text-align: center;
+`
+
+export const LinkStyled = styled(NavLink)`
+display: inline-block;
+font-weight: 700;`
--- src/pages/Home/components/HowItWorksSection/HowItWorks.tsx
+++ src/pages/Home/components/HowItWorksSection/HowItWorks.tsx
@@ -2,9 +2,17 @@
 // import step1Img from "../assets/steps/step1.png";
 // import step2Img from "../assets/steps/step2.png";
 // import step3Img from "../assets/steps/step3.png";
-import { ContentContainer, SectionStyled, SectionTitle, ListStyled, ItemStyled } from "./HowItWorks.styled";
+import {
+  ContentContainer,
+  SectionStyled,
+  SectionTitle,
+  ListStyled,
+  ItemStyled,
+  SvgStyled,
+  TextStyled,
+  LinkStyled
+} from "./HowItWorks.styled";
 import svgImg from "../../assets/homepage_vector-img.svg";
-import { NavLink } from "react-router-dom";
 
 const HowItWorks = () => {
   return (
@@ -13,30 +21,30 @@ const HowItWorks = () => {
         <SectionTitle>Як це працює?</SectionTitle>
         <ListStyled>
           <ItemStyled>
-            <svg>
+            <SvgStyled>
               <use href={`${svgImg}#howworks_fill`}></use>
-            </svg>
-            <p>
-              <NavLink to="/subscription">Заповнюйте анкету</NavLink> на сайті або у Дитячій точці "Спільно”, де вже працює бібліотека
-              іграшок
-            </p>
+            </SvgStyled>
+            <TextStyled>
+              <LinkStyled to="/subscription">Заповнюйте анкету</LinkStyled> на сайті або у Дитячій точці
+              "Спільно”, де вже працює бібліотека іграшок
+            </TextStyled>
           </ItemStyled>
           <ItemStyled>
-            <p>
-              <svg>
-                <use href={`${svgImg}#howworks_fill`}></use>
-              </svg>
+            <SvgStyled>
+              <use href={`${svgImg}#howworks_fill`}></use>
+            </SvgStyled>
+            <TextStyled>
               Відвідуйте ігрові сесії у Дитячій точці "Спільно” та забираєте іграшки, що
               сподобались, додому
-            </p>
+            </TextStyled>
           </ItemStyled>
           <ItemStyled>
-            <p>
-              <svg>
-                <use href={`${svgImg}#howworks_takepart`}></use>
-              </svg>
+            <SvgStyled>
+              <use href={`${svgImg}#howworks_takepart`}></use>
+            </SvgStyled>
+            <TextStyled>
               Обмінюйте іграшки щотримісяці та ставайте частиною спільноти однодумців
-            </p>
+            </TextStyled>
           </ItemStyled>
         </ListStyled>
       </ContentContainer>
