6bce3f1f0350a959546200c8973eefdc582e38ed
VitaliiR <104318952+Vi7ality@users.noreply.github.com>
2024-02-21 17:34:41 +0200

    Whale adaptive styles added

--- src/shared/Footer/Footer.styled.ts
+++ src/shared/Footer/Footer.styled.ts
@@ -1,18 +1,38 @@
 import styled from "styled-components";
-import whaleBg from './assets/whale-bg.svg'
+import whaleBg from "./assets/whale-bg.svg";
 
 export const FooterStyled = styled.footer`
-margin: 0;
-padding-top: 289px;
-height: auto;
-&::before {
-    content: '';
+  margin: 0;
+  padding-top: 146px;
+  height: auto;
+
+  @media ${(props) => props.theme.device.tablet} {
+    padding-top: 189px;
+  }
+
+  @media ${(props) => props.theme.device.desktop} {
+    padding-top: 289px;
+  }
+  &::before {
+    content: "";
     background-image: url(${whaleBg});
-        background-repeat: no-repeat;
-            background-position: center;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: 711px 119px;
     position: absolute;
     top: 0;
     width: 100%;
-    height: 288px;
+    height: 119px;
 
-}`
+    @media ${(props) => props.theme.device.tablet} {
+      background-size: 1135px 189px;
+      height: 189px;
+    }
+
+    @media ${(props) => props.theme.device.desktop} {
+      background-size: 1727px 289px;
+      height: 288px;
+      width: 100%;
+    }
+  }
+`;
