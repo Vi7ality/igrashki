cdc1f8204d4e3fd8d15a3ec317b920e78f0cabfd
VitaliiR <104318952+Vi7ality@users.noreply.github.com>
2024-03-28 12:33:12 +0200

    classes cleared

--- src/pages/Subscription/components/AuthSection/AuthSection.styled.ts
+++ src/pages/Subscription/components/AuthSection/AuthSection.styled.ts
@@ -1,1 +1,16 @@
 import styled from "styled-components";
+
+export const SectionStyled = styled.section`
+border-radius: 15px 15px 0 0;
+padding: 30px 20px;
+background-color: ${p => p.theme.colors.white};
+
+@media ${p => p.theme.device.tablet} {
+border-radius: 0 30px 30px 0;
+padding: 40px 42px;
+}
+
+@media ${p => p.theme.device.desktop} {
+    
+}
+`
--- src/pages/Subscription/components/AuthSection/index.tsx
+++ src/pages/Subscription/components/AuthSection/index.tsx
@@ -2,27 +2,28 @@ import Register from "../Register";
 import styles from "../../Subscription.module.scss";
 import { Formik } from "formik";
 import { FormEvent } from "react";
+import { SectionStyled } from "./AuthSection.styled";
 
 type ClientValuesType = {
-  parentName: string,
-  parentSurname: string,
-  phoneNumber: string,
-  password: string,
-  childName: string,
-  childBirthDate: string,
-  messenger: "telegram" | "viber",
-  formType: "online" | "offline",
-  acceptRules: boolean,
-}
+  parentName: string;
+  parentSurname: string;
+  phoneNumber: string;
+  password: string;
+  childName: string;
+  childBirthDate: string;
+  messenger: "telegram" | "viber";
+  formType: "online" | "offline";
+  acceptRules: boolean;
+};
 
 type PropType = {
-  handleSubmit: (e: FormEvent) => Promise<void>,
-  selectedManagementPoint: any,
-  client: any,
-  clientValues: ClientValuesType,
-  setClientValues(values: ClientValuesType): void,
-  setIsAuthModalOpen(value: boolean): void
-}
+  handleSubmit: (e: FormEvent) => Promise<void>;
+  selectedManagementPoint: any;
+  client: any;
+  clientValues: ClientValuesType;
+  setClientValues(values: ClientValuesType): void;
+  setIsAuthModalOpen(value: boolean): void;
+};
 
 const AuthSection = ({
   handleSubmit,
@@ -33,97 +34,91 @@ const AuthSection = ({
   setIsAuthModalOpen,
 }: PropType) => {
   return (
-    <>
-      {/* {!client?._id && <img className={styles.animalCenter} src={animalCenter} alt="animal" />} */}
-      <div className={styles.leftContent}>
-        <Formik
-          initialValues={{parentName: ''}}
-          onSubmit={handleSubmit}>{({ handleSubmit, getFieldProps, touched, errors }) => {
-            return (
-              <form onSubmit={handleSubmit} className={styles.subscriptionForm} id="formId">
-          <div className={styles.field}>
-            {selectedManagementPoint && (
-                <label htmlFor="managementPoint" className={styles.fieldLabel}>
-                  Локація
-                </label>
-              ) && (
-                <input
-                  className={styles.fieldInput}
-                  value={selectedManagementPoint ? selectedManagementPoint.location : ""}
-                  disabled
-                />
+    <SectionStyled>
+      <Formik initialValues={{ parentName: "" }} onSubmit={handleSubmit}>
+        {({ handleSubmit, getFieldProps, touched, errors }) => {
+          return (
+            <form onSubmit={handleSubmit} id="formId">
+              <div>
+                {selectedManagementPoint && (
+                  <>
+                    <label htmlFor="managementPoint">
+                      Локація
+                    </label>
+                    <input
+                      value={selectedManagementPoint ? selectedManagementPoint.location : ""}
+                      disabled
+                    />
+                  </>
+                )}
+              </div>
+              {!client?._id ? (
+                <Register clientValues={clientValues} setClientValues={setClientValues} />
+              ) : (
+                <>
+                  <div>
+                    <label htmlFor="parentName">
+                      Ім'я
+                    </label>
+                    <input
+                      type="text"
+                      name="parentName"
+                      className={styles.fieldInput}
+                      placeholder="Ім'я"
+                      value={client?.parentName}
+                      disabled
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="parentSurname">
+                      Прізвище
+                    </label>
+                    <input
+                      type="text"
+                      name="parentSurname"
+                      placeholder="Прізвище"
+                      value={client?.parentSurname}
+                      disabled
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="phoneNumber">
+                      Мобільний номер
+                    </label>
+                    <input
+                      type="tel"
+                      name="phoneNumber"
+                      placeholder="+38 (___) ___ __ __"
+                      value={client?.phoneNumber}
+                      disabled
+                    />
+                  </div>
+                </>
               )}
-          </div>
-          {!client?._id ? (
-            <Register
-              clientValues={clientValues}
-              setClientValues={setClientValues}
-                  />
-          ) : (
-            <>
-              <div className={styles.field}>
-                <label htmlFor="parentName" className={styles.fieldLabel}>
-                  Ім'я
-                </label>
-                <input
-                  type="text"
-                  name="parentName"
-                  className={styles.fieldInput}
-                  placeholder="Ім'я"
-                  value={client?.parentName}
-                  disabled
-                />
-              </div>
-              <div className={styles.field}>
-                <label htmlFor="parentSurname" className={styles.fieldLabel}>
-                  Прізвище
-                </label>
-                <input
-                  type="text"
-                  name="parentSurname"
-                  className={styles.fieldInput}
-                  placeholder="Прізвище"
-                  value={client?.parentSurname}
-                  disabled
-                />
-              </div>
-              <div className={styles.field}>
-                <label htmlFor="phoneNumber" className={styles.fieldLabel}>
-                  Мобільний номер
-                </label>
-                <input
-                  type="tel"
-                  name="phoneNumber"
-                  className={styles.fieldInput}
-                  placeholder="+38 (___) ___ __ __"
-                  value={client?.phoneNumber}
-                  disabled
-                />
-              </div>
-            </>
-          )}
-          <button
-            disabled={!clientValues.acceptRules && !client?._id}
-            type="submit"
-            className={styles.btnSubmit}
-          >
-            Відправити
-          </button>
-        </form>
-          )} }</Formik>
-      {!client?._id && <div className={styles.headerSub}>
-        <>
-          <p>
-            Якщо ви вже заповнили анкету у одній з бібліотек або онлайн — увійдіть до свого
-            облікового запису, натиснувши кнопку нижче
-          </p>
-          <button onClick={() => setIsAuthModalOpen(true)} className={styles.accountBtn}>
-            Увійти
-          </button>
-        </>
-      </div>}
-      </div>
-    </>
+              <button
+                disabled={!clientValues.acceptRules && !client?._id}
+                type="submit"
+              >
+                Відправити
+              </button>
+            </form>
+          );
+        }}
+      </Formik>
+      {!client?._id && (
+        <div className={styles.headerSub}>
+          <>
+            <p>
+              Якщо ви вже заповнили анкету у одній з бібліотек або онлайн — увійдіть до свого
+              облікового запису, натиснувши кнопку нижче
+            </p>
+            <button onClick={() => setIsAuthModalOpen(true)} className={styles.accountBtn}>
+              Увійти
+            </button>
+          </>
+        </div>
+      )}
+    </SectionStyled>
   );
 };
 
