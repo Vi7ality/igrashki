7a97ef3154eab79007c920d8912651db2eefaddc
VitaliiR <104318952+Vi7ality@users.noreply.github.com>
2024-04-05 20:51:03 +0300

    scss styles cleared

--- src/pages/Catalogue/index.tsx
+++ src/pages/Catalogue/index.tsx
@@ -1,41 +1,41 @@
-import Footer from "../../shared/Footer";
-import Header from "../../shared/Header";
-import styles from "./Catalogue.module.scss";
-import { Link } from "react-router-dom";
-import { useAppDispatch, useAppSelector } from "../../redux/store";
-import { ChangeEvent, useEffect, useMemo, useState } from "react";
-import { fetchToys } from "../../redux/slices/toys.slice";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import Footer from '../../shared/Footer';
+import Header from '../../shared/Header';
+import styles from './Catalogue.module.scss';
+import { Link } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from '../../redux/store';
+import { ChangeEvent, useEffect, useMemo, useState } from 'react';
+import { fetchToys } from '../../redux/slices/toys.slice';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import {
   addItemToCart,
   setSelectedManagementPoint,
-} from "../../redux/slices/cart.slice";
-import api from "../../api";
-import { IManager } from "../../models/manager";
-import { IToyInfo } from "../../models/toy";
+} from '../../redux/slices/cart.slice';
+import api from '../../api';
+import { IManager } from '../../models/manager';
+import { IToyInfo } from '../../models/toy';
+import HeaderBackgound from '../../shared/HeaderBackground';
+import Container from '../../shared/Container';
 
 const Catalogue = () => {
   const { toys, isLoading: isToysLoading } = useAppSelector(
-    (state) => state.toys
+    state => state.toys
   );
-  const { cart, selectedManagementPoint } = useAppSelector(
-    (state) => state.cart
-  );
+  const { cart, selectedManagementPoint } = useAppSelector(state => state.cart);
   const [managementPoints, setManagementPoints] = useState([]);
-  const [selectedCity, setSelectedCity] = useState<string>("");
+  const [selectedCity, setSelectedCity] = useState<string>('');
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     const fetchPoints = async () => {
-      const { data } = await api.get("/management/points");
+      const { data } = await api.get('/management/points');
       setManagementPoints(data);
 
       dispatch(setSelectedManagementPoint(data[0]));
       setSelectedCity(data[0].city);
     };
     fetchPoints();
-  }, []);
+  }, [dispatch]);
 
   useEffect(() => {
     selectedManagementPoint && dispatch(fetchToys(selectedManagementPoint));
@@ -43,11 +43,11 @@ const Catalogue = () => {
 
   const handleAddToCart = (toy: IToyInfo) => {
     if (toy) {
-      const isItemInCart = cart.some((item) => item.itemId === toy.toyId);
+      const isItemInCart = cart.some(item => item.itemId === toy.toyId);
       if (isItemInCart) {
-        toast.warning("Іграшка вже додана в кошик!", { autoClose: 1000 });
+        toast.warning('Іграшка вже додана в кошик!', { autoClose: 1000 });
       } else if (cart.length >= 3) {
-        toast.error("Кошик повний. Максимальна кількість: 3 іграшки.", {
+        toast.error('Кошик повний. Максимальна кількість: 3 іграшки.', {
           autoClose: 1000,
         });
       } else {
@@ -58,14 +58,14 @@ const Catalogue = () => {
             itemImage: toy.images[0],
           })
         );
-        toast.success("Іграшку додано в кошик!", { autoClose: 1000 });
+        toast.success('Іграшку додано в кошик!', { autoClose: 1000 });
       }
     }
   };
 
   const handleCitySelect = (e: ChangeEvent<HTMLSelectElement>) => {
     if (cart.length > 0) {
-      toast.warning("Очистіть кошик перед зміною міста отримання!", {
+      toast.warning('Очистіть кошик перед зміною міста отримання!', {
         autoClose: 3000,
       });
       return;
@@ -80,7 +80,7 @@ const Catalogue = () => {
 
   const handleChangeLocation = (e: ChangeEvent<HTMLSelectElement>) => {
     if (cart.length > 0) {
-      toast.warning("Очистіть кошик перед вибором іншої локації!", {
+      toast.warning('Очистіть кошик перед вибором іншої локації!', {
         autoClose: 3000,
       });
       return;
@@ -105,69 +105,74 @@ const Catalogue = () => {
   );
 
   return (
-    <div className={styles.catalogue}>
-      <Header />
-      <div className={styles.сontent}>
-        <h1>Каталог іграшок</h1>
-        <div className={styles.container}>
-          <aside>
-            <div className={styles.location}>
-              <h3>Місто</h3>
-              {cities.length > 0 && (
-                <select
-                  className={styles.locationSelect}
-                  value={selectedCity}
-                  onChange={handleCitySelect}
-                >
-                  {cities.map((city: string) => (
-                    <option key={city} value={city}>
-                      {city}
-                    </option>
-                  ))}
-                </select>
-              )}
-            </div>
-            <div className={styles.location}>
-              <h3>Локація</h3>
-              {managementPointsByCity.length > 0 && (
-                <select
-                  className={styles.locationSelect}
-                  value={selectedManagementPoint?._id}
-                  onChange={(e) => handleChangeLocation(e)}
-                >
-                  {managementPointsByCity.map(({ _id, location }: IManager) => (
-                    <option key={_id} value={_id}>
-                      {location}
-                    </option>
-                  ))}
-                </select>
-              )}
-            </div>
-          </aside>
-          <main className={styles.main}>
-            {isToysLoading ? (
-              <h1>Шукаємо іграшки на вибраній локації...</h1>
-            ) : (
-              <div className={styles.toys}>
-                {toys.map((toy) => (
-                  <div className={styles.toy} key={toy._id}>
-                    <Link className={styles.toyImage} to={`/toys/${toy.toyId}`}>
-                      <img src={toy?.images[0]} alt="toy" />
-                    </Link>
-                    <h6 className={styles.toyName}>{toy.toyName}</h6>
-                    <button
-                      className={styles.toyButton}
-                      onClick={() => handleAddToCart(toy)}
-                    >
-                      Додати
-                    </button>
-                  </div>
-                ))}
+    <div>
+      <HeaderBackgound />
+      <section>
+        <Container>
+        <div>
+          <h1>Каталог іграшок</h1>
+          <div>
+            <aside>
+              <div>
+                <h3>Місто</h3>
+                {cities.length > 0 && (
+                  <select
+                    value={selectedCity}
+                    onChange={handleCitySelect}
+                  >
+                    {cities.map((city: string) => (
+                      <option key={city} value={city}>
+                        {city}
+                      </option>
+                    ))}
+                  </select>
+                )}
               </div>
-            )}
-          </main>
-        </div>
-      </div>
+              <div>
+                <h3>Локація</h3>
+                {managementPointsByCity.length > 0 && (
+                  <select
+                    value={selectedManagementPoint?._id}
+                    onChange={e => handleChangeLocation(e)}
+                  >
+                    {managementPointsByCity.map(
+                      ({ _id, location }: IManager) => (
+                        <option key={_id} value={_id}>
+                          {location}
+                        </option>
+                      )
+                    )}
+                  </select>
+                )}
+              </div>
+            </aside>
+              {isToysLoading ? (
+                <h1>Шукаємо іграшки на вибраній локації...</h1>
+              ) : (
+                <div>
+                  {toys.map(toy => (
+                    <div>
+                      <Link
+                        to={`/toys/${toy.toyId}`}
+                      >
+                        <img src={toy?.images[0]} alt="toy" />
+                      </Link>
+                      <h6>{toy.toyName}</h6>
+                      <button
+                        onClick={() => handleAddToCart(toy)}
+                      >
+                        Додати
+                      </button>
+                    </div>
+                  ))}
+                </div>
+              )}
+          </div>
+          </div>
+          </Container>
+      </section>
+
+
       <ToastContainer
         position="bottom-right"
         hideProgressBar={false}
@@ -180,8 +185,6 @@ const Catalogue = () => {
         theme="light"
         closeButton={false}
       />
-
-      <Footer />
     </div>
   );
 };
