7d2aef085b8aeef0415a95119d853bd0a809866c
VitaliiR <104318952+Vi7ality@users.noreply.github.com>
2024-04-08 14:00:00 +0300

    drafts cleared

--- src/pages/Catalogue/components/Checkbox.tsx
+++
@@ -1,9 +0,0 @@
-import CheckboxIcon from "./icons/CheckboxIcon";
-import CheckboxChecked from "./icons/CheckboxChecked";
-
-const Checkbox = ({ checked }: { checked?: boolean }) => {
-  const Icon = checked ? <CheckboxChecked /> : <CheckboxIcon />;
-  return <>{Icon}</>;
-};
-
-export default Checkbox;
