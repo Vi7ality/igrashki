3212fa418441c1a56e425a20cb135ed0d3d51dc3
VitaliiR <104318952+Vi7ality@users.noreply.github.com>
2024-05-08 12:27:03 +0300

    og tags addded

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,7 @@ import HowItWorks from './components/HowItWorksSection';
 import WhyUs from './components//WhyUs/WhyUs';
 import HeroSection from './components/HeroSection/HeroSection';
 import JoinUs from './components/JoinUs';
+import logo from "../../assets/logo.svg";
 import { lazy } from 'react';
 import { Helmet } from 'react-helmet';
 const Gallery = lazy(()=> import('./components/GallerySection/Gallery'))
@@ -16,8 +17,14 @@ const Home = () => {
         </title>
         <meta
           name="description"
-          content="Це програма з безкоштовної оренди дитячих іграшок для раннього розвитку, що реалізується через мережу Дитячих точок Спільно. В основі проєкту лежить ідея бібліотеки, де замість книжок — іграшки.Наразі проєкт реалізується у Чернівецькій, Тернопільській, Івано-Франківській та Закарпатській та Львівській областях."
+          content="Програма з безкоштовної оренди дитячих іграшок для раннього розвитку, що реалізується через мережу Дитячих точок Спільно. В основі проєкту лежить ідея бібліотеки, де замість книжок — іграшки.Наразі проєкт реалізується у Чернівецькій, Тернопільській, Івано-Франківській та Закарпатській та Львівській областях."
         />
+        <meta property='og:type' content='website'/>
+        <meta property='og:url' content='/'/>
+        <meta property='og:title' content='Спільно іграшки. Мережа безоплатних бібліотек іграшок для раннього
+          розвитку'/>
+        <meta property='og:description' content='Програма з безкоштовної оренди дитячих іграшок для раннього розвитку, що реалізується через мережу Дитячих точок Спільно.'/>
+        <meta property='og:image' content={logo}/>
       </Helmet>
       <HeroSection />
       <HowItWorks />
