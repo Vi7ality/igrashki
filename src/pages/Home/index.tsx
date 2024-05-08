8aa4790578a79bd4749c8d71ce5c4a37cd41ec04
VitaliiR <104318952+Vi7ality@users.noreply.github.com>
2024-05-08 11:57:00 +0300

    gallery lazy loading added

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,28 @@
-import HowItWorks from "./components/HowItWorksSection";
-import WhyUs from "./components//WhyUs/WhyUs";
-import HeroSection from "./components/HeroSection/HeroSection";
-import Gallery from "./components/GallerySection/Gallery";
-import JoinUs from "./components/JoinUs";
-
+import HowItWorks from './components/HowItWorksSection';
+import WhyUs from './components//WhyUs/WhyUs';
+import HeroSection from './components/HeroSection/HeroSection';
+import JoinUs from './components/JoinUs';
+import { lazy } from 'react';
+import { Helmet } from 'react-helmet';
+const Gallery = lazy(()=> import('./components/GallerySection/Gallery'))
 
 const Home = () => {
   return (
     <>
+      <Helmet>
+        <title>
+          Спільно іграшки. Мережа безоплатних бібліотек іграшок для раннього
+          розвитку
+        </title>
+        <meta
+          name="description"
+          content="Це програма з безкоштовної оренди дитячих іграшок для раннього розвитку, що реалізується через мережу Дитячих точок Спільно. В основі проєкту лежить ідея бібліотеки, де замість книжок — іграшки.Наразі проєкт реалізується у Чернівецькій, Тернопільській, Івано-Франківській та Закарпатській та Львівській областях."
+        />
+      </Helmet>
       <HeroSection />
       <HowItWorks />
       <WhyUs />
-      <Gallery/>
+      <Gallery />
       <JoinUs />
     </>
   );
--- src/shared/SharedLayout/index.tsx
+++ src/shared/SharedLayout/index.tsx
@@ -3,21 +3,9 @@ import Header from '../Header';
 import { Outlet } from 'react-router-dom';
 import Footer from '../Footer';
 import ScrollToTop from '../../utils/ScrollToTop';
-import { Helmet } from 'react-helmet';
 
 const SharedLayout = () => {
   return (
-    <>
-      <Helmet>
-        <title>
-          Спільно іграшки. Мережа безоплатних бібліотек іграшок для раннього
-          розвитку
-        </title>
-        <meta
-          name="description"
-          content="Це програма з безкоштовної оренди дитячих іграшок для раннього розвитку, що реалізується через мережу Дитячих точок Спільно. В основі проєкту лежить ідея бібліотеки, де замість книжок — іграшки.Наразі проєкт реалізується у Чернівецькій, Тернопільській, Івано-Франківській та Закарпатській та Львівській областях."
-        />
-      </Helmet>
       <div
         style={{
           minHeight: '100vh',
@@ -35,7 +23,6 @@ const SharedLayout = () => {
         </main>
         <Footer />
       </div>
-    </>
   );
 };
 
