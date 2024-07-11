97844bd530c4511efb9cb3154a4f4e8f344484e1
VitaliiR <104318952+Vi7ality@users.noreply.github.com>
2024-07-11 17:08:41 +0300

    Formik added to managerLogin component

--- src/pages/ManagerLogin/index.tsx
+++ src/pages/ManagerLogin/index.tsx
@@ -1,45 +1,73 @@
 import { IManagerCredentials } from '../../models/manager';
-import Input from '../../shared/formComponents/Input'
+import Input from '../../shared/formComponents/Input';
 import { useForm } from 'react-hook-form';
-import styles from './ManagerLogin.module.scss'
+import styles from './ManagerLogin.module.scss';
 import { useAppDispatch, useAppSelector } from '../../redux/store';
 import { managerLogin } from '../../redux/slices/manager.slice';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Form, Formik } from 'formik';
+import FormikInput from '../../shared/formComponents/FormikInput';
 
 const defaultValues = {
-    managerUserName: "",
-    managerPassword: ""
-}
+  managerUserName: '',
+  managerPassword: '',
+};
 
 const ManagerLogin = () => {
-    const navigate = useNavigate();
-    const dispatch = useAppDispatch();
-    const {currentManager} = useAppSelector(state => state.manager)
-    const { register, handleSubmit } = useForm<IManagerCredentials>(
-        { mode: 'onBlur', values: defaultValues }
-    );
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const { currentManager } = useAppSelector(state => state.manager);
+  const { register, handleSubmit } = useForm<IManagerCredentials>({
+    mode: 'onBlur',
+    values: defaultValues,
+  });
 
-    useEffect(() => {
-        if (currentManager?._id) {
-            navigate('/management')
-        }
-    }, [currentManager?._id])
+  useEffect(() => {
+    if (currentManager?._id) {
+      navigate('/management');
+    }
+  }, [currentManager?._id]);
 
-    const onSubmit = (data: IManagerCredentials) => {
-        dispatch(managerLogin(data))
-    };
+  const handleLogin = (data: IManagerCredentials) => {
+    dispatch(managerLogin(data));
+  };
 
-    return (
-        <div className={styles.managerForm}>
-            <h1>Manager Login</h1>
-            <form onSubmit={handleSubmit(onSubmit)}>
+  return (
+    <div className={styles.managerForm}>
+      <h1>Manager Login</h1>
+      <Formik
+        onSubmit={handleLogin}
+        initialValues={{ managerUserName: '', managerPassword: '' }}
+      >
+        {({ handleSubmit, getFieldProps }) => {
+          return (
+            <Form onSubmit={handleSubmit}>
+              <FormikInput
+                label="Username"
+                {...register('managerUserName')}
+                name="managerUserName"
+                getFieldProps={getFieldProps}
+              />
+              <FormikInput
+                label="Password"
+                {...register('managerPassword')}
+                name="managerPassword"
+                type="password"
+                getFieldProps={getFieldProps}
+              />
+              <button type="submit">Login</button>
+            </Form>
+          );
+        }}
+      </Formik>
+      {/* <form onSubmit={handleSubmit(onSubmit)}>
                 <Input label="Username"  {...register('managerUserName')} name="managerUserName" />
                 <Input label="Password"  {...register('managerPassword')} name="managerPassword" type='password' />
                 <button type="submit">Login</button>
-            </form>
-        </div>
-    )
-}
+            </form> */}
+    </div>
+  );
+};
 
-export default ManagerLogin
+export default ManagerLogin;
