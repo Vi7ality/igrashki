0a437a554451a8fe922f11759b23a6dceaab4f67
VitaliiR <104318952+Vi7ality@users.noreply.github.com>
2024-07-11 17:23:20 +0300

    spinner added

--- src/pages/ManagerLogin/index.tsx
+++ src/pages/ManagerLogin/index.tsx
@@ -1,6 +1,4 @@
 import { IManagerCredentials } from '../../models/manager';
-import Input from '../../shared/formComponents/Input';
-import { useForm } from 'react-hook-form';
 import styles from './ManagerLogin.module.scss';
 import { useAppDispatch, useAppSelector } from '../../redux/store';
 import { managerLogin } from '../../redux/slices/manager.slice';
@@ -9,20 +7,27 @@ import { useNavigate } from 'react-router-dom';
 import { Form, Formik } from 'formik';
 import FormikInput from '../../shared/formComponents/FormikInput';
 import { managerLoginValidSchema } from '../../utils/validationSchemas/authValidationSchema';
+import LoadSpinner from '../../shared/LoadSpinner';
 
 const defaultValues = {
   managerUserName: '',
   managerPassword: '',
 };
 
+const spinnerSettings = {
+  height: '48',
+  width: '48',
+  color: 'white',
+};
+
 const ManagerLogin = () => {
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
-  const { currentManager } = useAppSelector(state => state.manager);
-  const { register, handleSubmit } = useForm<IManagerCredentials>({
-    mode: 'onBlur',
-    values: defaultValues,
-  });
+  const {
+    currentManager,
+    error,
+    loading: isLoading,
+  } = useAppSelector(state => state.manager);
 
   useEffect(() => {
     if (currentManager?._id) {
@@ -39,35 +44,38 @@ const ManagerLogin = () => {
       <h1>Manager Login</h1>
       <Formik
         onSubmit={handleLogin}
-              initialValues={{ managerUserName: '', managerPassword: '' }}
-              validationSchema={managerLoginValidSchema}
+        initialValues={defaultValues}
+        validationSchema={managerLoginValidSchema}
       >
         {({ handleSubmit, getFieldProps }) => {
           return (
             <Form onSubmit={handleSubmit}>
               <FormikInput
                 label="Username"
-                {...register('managerUserName')}
                 name="managerUserName"
                 getFieldProps={getFieldProps}
               />
               <FormikInput
                 label="Password"
-                {...register('managerPassword')}
                 name="managerPassword"
                 type="password"
                 getFieldProps={getFieldProps}
               />
-              <button type="submit">Login</button>
+              <button type="submit">
+                {!isLoading ? (
+                  'Login'
+                ) : (
+                  <LoadSpinner
+                    height={spinnerSettings.height}
+                    width={spinnerSettings.width}
+                    color={spinnerSettings.color}
+                  />
+                )}
+              </button>
             </Form>
           );
         }}
       </Formik>
-      {/* <form onSubmit={handleSubmit(onSubmit)}>
-                <Input label="Username"  {...register('managerUserName')} name="managerUserName" />
-                <Input label="Password"  {...register('managerPassword')} name="managerPassword" type='password' />
-                <button type="submit">Login</button>
-            </form> */}
     </div>
   );
 };
