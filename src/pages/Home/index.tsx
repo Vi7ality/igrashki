efa5e1b7fb0b2396d0ea20d0ea17698f6b656f0e
VitaliiR <104318952+Vi7ality@users.noreply.github.com>
2024-07-19 23:51:06 +0300

    update og img

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,6 +7,7 @@ import { Helmet } from 'react-helmet';
 const Gallery = lazy(()=> import('./components/GallerySection/Gallery'))
 
 const Home = () => {
+  const websiteURL = window.location.href;
   
   return (
     <>
@@ -24,7 +25,7 @@ const Home = () => {
         <meta property='og:title' content='Спільно іграшки. Мережа безоплатних бібліотек іграшок для раннього
           розвитку'/>
         <meta property='og:description' content='Програма з безкоштовної оренди дитячих іграшок для раннього розвитку, що реалізується через мережу Дитячих точок Спільно.'/>
-        <meta property='og:image' content="/dist/growithyou-og.jpg"/>
+        <meta property='og:image' content={websiteURL + "dist/growithyou-og.jpg"}/>
       </Helmet>
       <HeroSection />
       <HowItWorks />
